d9031b4ee3a02b9e1a30f0e7d296df785d274f4e
Kenneth <kennethtenny10@gmail.com>
2017-04-14 00:30:30 +0530

    Playing from database with public auth change.

--- VideoPlay/app/src/main/java/com/example/kennymac/videoplay/MainActivity.java
+++ VideoPlay/app/src/main/java/com/example/kennymac/videoplay/MainActivity.java
@@ -1,32 +1,106 @@
 package com.example.kennymac.videoplay;
 
+import android.media.MediaPlayer;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.MediaController;
+import android.widget.ProgressBar;
 import android.widget.VideoView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private StorageReference mStorageRef;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        String mUrl = "http://www.servername.com/projects/projectname/videos/1361439400.mp4";
-//
-//        Uri uri = Uri.parse("https://drive.google.com/open?id=0BwpSkSc58LtoLVN1d0RObXU5YW8"); //Declare your url here.
-//
-//        VideoView mVideoView = (VideoView) findViewById(R.id.videoview);
-//        mVideoView.setMediaController(new MediaController(this));
-//        mVideoView.setVideoURI(uri);
-//        mVideoView.requestFocus();
-//        mVideoView.start();
+        mAuth = FirebaseAuth.getInstance();
+
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d("TAG", "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d("TAG", "onAuthStateChanged:signed_out");
+                }
+
+            }
+        };
+        
+
+        String str="https://firebasestorage.googleapis.com/v0/b/videoplay-e370a.appspot.com/o/Nikon%20D7500-%20Time-lapse%20sample%20video.mp4?alt=media&token=58cdbd51-a96f-481c-80b1-b755928ace58";
+        Uri uri = Uri.parse(str);
+
+
 
         ImageView mImageView= (ImageView)findViewById(R.id.ImageView);
         VideoView mVideoView= (VideoView)findViewById(R.id.VideoView);
+        final ProgressBar progressBarLandScape=(ProgressBar)findViewById(R.id.progressbar);
+
+        mVideoView.setVideoURI(uri);
+        progressBarLandScape.setVisibility(View.VISIBLE);
+        mVideoView.requestFocus();
+        mVideoView.start();
+
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true);
+            }
+        });
+
+        mVideoView.setOnInfoListener(new MediaPlayer.OnInfoListener() {
+            @Override
+            public boolean onInfo(MediaPlayer mp, int what, int extra) {
+                if (what == MediaPlayer.MEDIA_INFO_BUFFERING_END) {
+                    return true;
+                }
+                else if(what == MediaPlayer.MEDIA_INFO_BUFFERING_START){
+                    return true;
+                }
+                return false;
+            }
+        });
+
+
 
         // check your conditions like show video view r image view here
 
@@ -34,15 +108,18 @@ public class MainActivity extends AppCompatActivity {
         MediaController mediaController = new MediaController(this);
         mediaController.setAnchorView(mVideoView);
         mVideoView.setMediaController(mediaController);
-        mVideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.sample));
+        //mVideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.sample));
+        mVideoView.setVideoURI(uri);
+
         mVideoView.start();
 
-        if(false)
+        if(true)
         {
             mVideoView.setVisibility(View.VISIBLE);
             mImageView.setVisibility(View.GONE);
+            progressBarLandScape.setVisibility(View.GONE);
         }
-        else if(true)
+        else if(false)
         {
             mImageView.setVisibility(View.VISIBLE);
             mVideoView.setVisibility(View.GONE);
